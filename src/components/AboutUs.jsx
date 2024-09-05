e88e5dd3e2adf0f1435d8bb32dc29e123115fedb
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-09-05 18:34:52 +0000

    edit  the aboutus component

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -10,17 +10,17 @@ function AboutUs() {
         <h1 className="font-bold text-5xl text-center tracking-tight mb-5">Why FundWave?</h1>
             <div className="flex flex-col md:flex-row gap-5">
                 <div className="flex flex-col items-center">
-                    <IoShieldOutline className="w-36 h-36 text-green-400"/>
+                    <IoShieldOutline className="w-28 h-28 text-green-400"/>
                     <h1 className="font-semibold text-xl tracking-normal mb-4">Strict Vetting</h1>
                     <p className="text-center text-sm font-medium text-black/50">We througly verify all campaigns to ensure your donation go to legitimate cause</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <IoMdCheckmarkCircleOutline className="w-36 h-36 text-green-400"/>
+                    <IoMdCheckmarkCircleOutline className="w-28 h-28 text-green-400"/>
                     <h1 className="font-semibold text-xl tracking-normal mb-4">MileStone-Based Funding</h1>
                     <p className="text-center text-sm font-medium text-black/50">Funds are released in stages, ensuring project stay on track and accountable</p>
                 </div>
                 <div className="flex flex-col items-center">
-                    <BsPeopleFill className="w-36 h-36 text-green-400"/>
+                    <BsPeopleFill className="w-28 h-28 text-green-400"/>
                     <h1 className="font-semibold text-xl tracking-normal mb-4">Community Engagement</h1>
                     <p className="text-center text-sm font-medium text-black/50">Connect dierectly with project creators and fellow donor to maximize impact</p>
                 </div>
