ab544185b3eaf572f042fb650cff6eec9f833b93
walonjalloh <107252572+walonjalloh@users.noreply.github.com>
2024-10-19 04:28:11 +0000

    Update corsOption.js

--- Backend/configs/corsOption.js
+++ Backend/configs/corsOption.js
@@ -1,5 +1,5 @@
 // configs/corsOption.js
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173','https://fundwave-frontend.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173','https://fundwave-frontend.vercel.app', 'https://fundwave-theta.vercel.app'];
 
 const corsOption = {
   origin: (origin, callback) => {
